0255f0a3b05ecc97b69443b0e7eb4d7427040507
zow2023 <125123077+zow2023@users.noreply.github.com>
2026-03-17 11:01:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,7 +21,6 @@ git clone https://github.com/w9315273/luci-app-adguardhome package/luci-app-adgu
 
 # git clone https://github.com/sbwml/luci-app-filemanager package/luci-app-filemanager
 
-#git clone https://github.com/animegasan/luci-app-quickstart package/luci-app-quickstart
 ##git clone https://github.com/brvphoenix/luci-app-wrtbwmon package/luci-app-wrtbwmon
 
 git clone https://github.com/gSpotx2f/luci-app-cpu-status package/luci-app-cpu-status
@@ -32,20 +31,20 @@ git clone https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netp
 #git clone https://github.com/muink/luci-app-tinyfilemanager package/luci-app-tinyfilemanager
 
 # 移除 openwrt feeds 自带的核心包
-rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
+#rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
 
 #rm -rf feeds/luci/applications/luci-app-dae
 #rm -rf feeds/luci/applications/luci-app-daed
 #rm -rf feeds/luci/applications/luci-app-homeproxy
 #rm -rf feeds/packages/net/{dae,daed}
 
-#git clone https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
+git clone https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
 
 #git clone https://github.com/Pacalini/luci-app-dae package/dae
 #git clone https://github.com/zow2023/luci-app-dae package/dae
 git clone https://github.com/QiuSimons/luci-app-dae -b kix package/dae
 
-git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
+#git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
 #rm -rf package/helloworld/luci-app-dae
 rm -rf package/helloworld/dae
 #rm -rf package/helloworld/shadowsocksr-libev
@@ -57,15 +56,7 @@ rm -rf package/helloworld/dae
 #rm -rf feeds/luci/applications/luci-app-smartdns  
 #git clone https://github.com/zow2023/openwrt-smartdns feeds/packages/net/smartdns
 #git clone https://github.com/pymumu/openwrt-smartdns feeds/packages/net/smartdns
-#git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
-
-#rm -rf feeds/luci/applications/luci-app-passwall
-#git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
-#git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci  
-
-#git clone https://github.com/fw876/helloworld.git -b master package/helloworld
-#rm -rf package/helloworld
-#git clone https://github.com/fw876/helloworld.git package/helloworld
+#git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns 
 
 #git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 #git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-theme-argon-config
